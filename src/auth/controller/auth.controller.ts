4edbfd2f632d9205bc50b88e83673a1e298ad4de
Santiago0212004 <sjbs0212@gmail.com>
2024-09-28 15:50:57 -0500

    fix: little fix

--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from '../service/auth.service';
 import { LoginUserDto, RegisterUserDto } from '../dto/auth.dto';
 
