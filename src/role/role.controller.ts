3e65a3c1b6ec1726f0f2ae215ee712c55dbc881a
Santiago0212004 <sjbs0212@gmail.com>
2024-10-07 13:20:32 -0500

    little change for deploy

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -6,7 +6,6 @@ import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 
-@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('roles')
 export class RoleController {
     constructor(private readonly roleService: RoleService) {}
