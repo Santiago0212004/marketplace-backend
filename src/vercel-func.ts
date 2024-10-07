cd4a22bca5268762f8d69d0d6d4dfc92cd317475
Santiago0212004 <sjbs0212@gmail.com>
2024-10-07 16:27:26 -0500

    fix some file routes

--- src/vercel-func.ts
+++ src/vercel-func.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AppModule } from './src/app.module';
+import { AppModule } from './app.module';
 import { HttpAdapterHost } from '@nestjs/core';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
