51af48056a857d15def9284cc7b28b3f4ee390a1
Santiago0212004 <sjbs0212@gmail.com>
2024-10-07 16:25:16 -0500

    created vercel-func.ts

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-
-let cachedServer: (arg0: any, arg1: any) => any;
 
 async function bootstrap() {
-  const expressApp = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
-
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
-
-  await app.init();
-  return expressApp;
+  await app.listen(process.env.PORT || 3000);
 }
-
-export default async function handler(req, res) {
-  if (!cachedServer) {
-    cachedServer = await bootstrap();
-  }
-  return cachedServer(req, res);
-}
+bootstrap();
