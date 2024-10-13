43b9bc44e246dd18355ad478440e69ce77c55afb
Santiago0212004 <sjbs0212@gmail.com>
2024-10-13 14:50:29 -0500

    fixed cors error

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,18 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(process.env.PORT || 3000);
+
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
