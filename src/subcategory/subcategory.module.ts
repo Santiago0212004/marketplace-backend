f8afa016052576192afc0cddfb5d0e169e73a36f
Santiago0212004 <sjbs0212@gmail.com>
2024-11-05 22:56:00 -0500

    hotfix: circular reference

--- src/subcategory/subcategory.module.ts
+++ src/subcategory/subcategory.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Subcategory } from './entity/subcategory.entity';
 import { SubcategoryController } from './subcategory.controller';
@@ -6,9 +6,10 @@ import { SubcategoryService } from './subcategory.service';
 import { CategoryModule } from '../category/category.module';
 import { User } from '../user/entity/user.entity';
 import { UserModule } from '../user/user.module';
+import { Category } from '../category/entity/category.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Subcategory, User]), CategoryModule, UserModule],
+    imports: [TypeOrmModule.forFeature([Subcategory, User, Category]), CategoryModule, forwardRef(()=>UserModule) ],
     exports: [TypeOrmModule],
     controllers: [SubcategoryController],
     providers: [SubcategoryService],
